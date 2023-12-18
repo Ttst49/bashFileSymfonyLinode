88f1a44d7e9bd1f9ddfdd1b7809e8065965ef824
Thibaut Stachnick <104855620+Ttst49@users.noreply.github.com>
2023-12-18 22:40:50 +0100

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -20,12 +20,6 @@ wget https://get.symfony.com/cli/installer -O - | bash
 curl -sS https://get.symfony.com/cli/installer | bash
 curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | sudo -E bash
 sudo apt install symfony-cli
-type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
 printf "<VirtualHost *:80>
         # The ServerName directive sets the request scheme, hostname and port that
         # the server uses to identify itself. This is used when creating
